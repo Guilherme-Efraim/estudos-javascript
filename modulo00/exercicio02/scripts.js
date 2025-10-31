346625e5ca42ac0f0f9dd88b103a3a272789e3ab
Guilherme-Efraim <guilhermeefraimdv@gmail.com>
2025-10-31 14:01:44 -0300

    atualizado 31/10/2025, 14:01

--- modulo00/exercicio02/scripts.js
+++ modulo00/exercicio02/scripts.js
@@ -3,8 +3,27 @@ const telaCalculo = document.getElementById("telaCalculo")
 
 const botoes = document.getElementById("botoes")
 const btnRecursos = document.getElementById("btnRecursos")
+
 const caixaNumeros = document.getElementById("caixaNumeros")
+const numeroCollection = document.querySelectorAll(".numeroMsm")
+
 const operadoresLogicos = document.getElementById("operadoresLogicos")
+
+/*-----------Recursos-------------*/
+
+const btnRecurso = ["Clear", "Remove"]
+
+btnRecurso.map((ele, posi) => {
+    const btnClear = document.createElement("button")
+    btnClear.setAttribute("id", "recurso"+posi)
+    btnClear.setAttribute("class", "btnRecurso")
+    btnClear.innerHTML=ele
+
+    btnRecursos.appendChild(btnClear)
+})
+console.log(btnRecursos)
+
+/*-----------Números-------------*/
 
 const btnNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -13,12 +32,28 @@ btnNumeros.map((ele, posi) => {
     btnAdicionado.setAttribute("class", "boxNum")
 
     const num = document.createElement("button")
-    num.setAttribute("id", "num"+posi)
+    num.setAttribute("id", "num" + posi)
     num.setAttribute("class", "numeroMsm")
     num.innerHTML=ele
 
     btnAdicionado.appendChild(num)
     caixaNumeros.appendChild(btnAdicionado)
-
 })
+
+/*-----------Operadores Lógicos-------------*/
+
+const operadoresArr = ["+", "-", "*", "%"]
+
+operadoresArr.map((ele, posi) => {
+    const operadorAdd = document.createElement("button")
+    operadorAdd.setAttribute("id", "operador" + posi)
+    operadorAdd.setAttribute("class", "operadores")
+    operadorAdd.innerHTML=ele
+
+    operadoresLogicos.appendChild(operadorAdd)
+})
+
+
+
+
 
